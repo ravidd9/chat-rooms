191517e22e3f695e52c7fe77f8bbd4bfc8190237
Ravid Cohen <ravidd9@gmail.com>
2019-06-16 16:47:23 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// [..]
+import {
+  handleInput,
+  connectToChatkit,
+} from './methods';
+import Dialog from './components/Dialog';
+
+import 'skeleton-css/css/normalize.css';
+import 'skeleton-css/css/skeleton.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userId: '',
+      showLogin: true,
+      isLoading: false,
+      currentUser: null,
+      currentRoom: null,
+      rooms: [],
+      roomUsers: [],
+      roomName: null,
+      messages: [],
+      newMessage: '',
+    }
+    this.handleInput = handleInput.bind(this);
+    this.connectToChatkit = connectToChatkit.bind(this);
+
+  }
+
+  render() {
+    const {
+      userId,
+      showLogin,
+      rooms,
+      currentRoom,
+      currentUser,
+      messages,
+      newMessage,
+      roomUsers,
+      roomName,
+    } = this.state;
+
+    return (
+      <div className="App">
+            <aside className="sidebar left-sidebar">
+              {currentUser ? (
+                <div className="user-profile">
+                  <span className="username">{currentUser.name}</span>
+                  <span className="user-id">{`@${currentUser.id}`}</span>
+                </div>
+              ) : null}
+            </aside>
+            <section className="chat-screen">
+             // [..]
+            </section>
+            <aside className="sidebar right-sidebar">
+              {showLogin ? (
+                <Dialog
+                  userId={userId}
+                  handleInput={this.handleInput}
+                  connectToChatkit={this.connectToChatkit}
+                />
+              ) : null}
+            </aside>
+          </div>
+    );
+  }
 }
 
 export default App;
